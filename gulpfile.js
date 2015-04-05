44b6f478c042156344a830eb93f91067c7eca82e
Oliver Lumby <me@oliverlumby.com>
2015-04-06 01:21:30 +0200

    use gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
+var path = require('path');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
 var notify = require('gulp-notify');
-var path = require('path');
 var webserver = require('gulp-webserver');
+var plumber = require('gulp-plumber');
+
 
 gulp.task('one', function () {
     gulp.src('./less/one.less')
+        .pipe(plumber())
         .pipe(less())
         .pipe(gulp.dest('css'))
         .pipe(notify("Example One Done"));
